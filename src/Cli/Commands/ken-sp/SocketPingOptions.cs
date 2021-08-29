929f552d202c91af3a5eaee6d655d7ffa3d45a40
kentxxq <admin@kentxxq.com>
2021-08-30 00:36:39 +0800

    feat: 采用host主机方式部署

---
+++ src/Cli/Commands/ken-sp/SocketPingOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Cli.Commands.ken_sp
+{
+    public class SocketPingOptions
+    {
+        public int RetryTimes { get; set; }
+        public int Timeout { get; set; }
+        public bool Quit { get; set; }
+    }
+}
--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 using Cli.Commands.ken_sp;
 using Cli.Commands.ken_ws;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Cli
 {
@@ -10,16 +15,43 @@ namespace Cli
 
         static async Task Main(string[] args)
         {
+#if DEBUG
+            var result = await BuildCommandLine()
+                  .UseHost(_ => Host.CreateDefaultBuilder(), builder =>
+                  {
+                      builder.ConfigureLogging(logging => logging
+                                                            .AddFilter("System", LogLevel.Error)
+                                                            .AddFilter("Microsoft", LogLevel.Error));
+                      builder.ConfigureServices(service =>
+                      {
+
+                      });
+                  })
+                  .UseDefaults()
+                  .Build()
+                  .InvokeAsync(new string[] { "sp", "baidu.com:443", "-t 2", "-n 10", });
+            System.Console.WriteLine(result);
+#else
+            await BuildCommandLine()
+                  .UseHost(_ => Host.CreateDefaultBuilder(), builder =>
+                  {
+                      builder.ConfigureServices(service =>
+                      {
+
+                      });
+                  })
+                  .UseDefaults()
+                  .Build()
+                  .InvokeAsync(args);
+#endif
+        }
+
+        private static CommandLineBuilder BuildCommandLine()
+        {
             var rootCommand = new RootCommand();
             rootCommand.AddCommand(SocketPingCommand.GetCommand());
             rootCommand.AddCommand(WebSocketCommand.GetCommand());
-#if DEBUG
-            //await rootCommand.InvokeAsync(new string[] { "sp", "baidu.com:443", "-n 3" });
-            //await rootCommand.InvokeAsync(new string[] { "ws", "wss://ws.kenxxq.com/ws" });
-            await rootCommand.InvokeAsync(new string[] { "ws", "wss://ws.kentxxq.com/ws" });
-#else
-            await rootCommand.InvokeAsync(args);
-#endif
+            return new CommandLineBuilder(rootCommand);
         }
     }
 }
