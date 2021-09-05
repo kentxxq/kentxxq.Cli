655a38b4a82bdce314c73051ab989bc97a68b463
kentxxq <admin@kentxxq.com>
2021-09-05 16:21:26 +0800

    feat: ss命令的基本参数

---
+++ src/Cli/Commands/ken-ss/SocketStatisticsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Cli.Commands.ken_ss
+{
+    class SocketStatisticsOptions
+    {
+        public bool Ipv4 { get; set; }
+        public bool Ipv6 { get; set; }
+        public bool Tcp { get; set; }
+        public bool Udp { get; set; }
+        public bool Listening { get; set; }
+        public bool Processes { get; set; }
+        public bool Numeric { get; set; }
+    }
+}
