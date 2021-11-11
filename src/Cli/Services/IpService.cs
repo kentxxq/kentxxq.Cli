3b67cee5dea3bf764454ffb865908da2d25ea397
kentxxq <admin@kentxxq.com>
2021-11-11 17:28:51 +0800

    feat: 启用json源生成，减少ILtrim警告

--- src/Cli/Services/IpService.cs
+++ src/Cli/Services/IpService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading;
 using System.Threading.Tasks;
 using Cli.Interfaces;
@@ -10,20 +11,20 @@ namespace Cli.Services
 {
     class IpService : IIpService
     {
-        public HttpClient _httpClient { get; }
+        public HttpClient httpClient { get; }
         public IpService(HttpClient client)
         {
             client.BaseAddress = new Uri("https://ip.taobao.com/outGetIpInfo");
-            _httpClient = client;
+            httpClient = client;
         }
 
         public async Task<IpInfo> GetIpInfoByIp(string ip)
         {
-            var result = await _httpClient.GetFromJsonAsync<IpInfo>($"?accessKey=alibaba-inc&&ip={ip}");
+            var result = await httpClient.GetFromJsonAsync<IpInfo>($"?accessKey=alibaba-inc&&ip={ip}", IpInfoContext.Default.IpInfo);
             if (result?.Code != Enums.IpInfoCode.个人qps超出)
             {
                 Thread.Sleep(1000);
-                result = await _httpClient.GetFromJsonAsync<IpInfo>($"?accessKey=alibaba-inc&&ip={ip}");
+                result = await httpClient.GetFromJsonAsync<IpInfo>($"?accessKey=alibaba-inc&&ip={ip}", IpInfoContext.Default.IpInfo);
             }
             return result ?? new IpInfo();
         }
