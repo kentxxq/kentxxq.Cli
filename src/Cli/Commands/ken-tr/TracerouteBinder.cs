425d46a085ff3cddebe57e5254f8702377a15cd1
kentxxq <805429509@qq.com>
2022-02-20 02:25:53 +0800

    style: 代码格式优化

--- src/Cli/Commands/ken-tr/TracerouteBinder.cs
+++ src/Cli/Commands/ken-tr/TracerouteBinder.cs
@@ -1,36 +1,27 @@
-﻿using Cli.Services;
-using System;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Binding;
 using System.Net.Http;
+using Cli.Services;
 
-namespace Cli.Commands.ken_tr
+namespace Cli.Commands.ken_tr;
+
+internal class TracerouteBinder : BinderBase<TracerouteType>
 {
-    internal class TracerouteBinder : BinderBase<TracerouteType>
+    private readonly Argument<string> _hostName;
+
+    public TracerouteBinder(Argument<string> hostName)
     {
-        private readonly Argument<string> _hostName;
+        _hostName = hostName;
+    }
 
-        public TracerouteBinder(Argument<string> hostName)
+    protected override TracerouteType GetBoundValue(BindingContext bindingContext)
+    {
+        var hostname = bindingContext.ParseResult.GetValueForArgument(_hostName);
+        return new TracerouteType
         {
-            _hostName = hostName;
-        }
-
-        protected override TracerouteType GetBoundValue(BindingContext bindingContext)
-        {
-            var hostname = bindingContext.ParseResult.GetValueForArgument(_hostName);
-            if (hostname == null)
-            {
-                throw new ArgumentException("参数错误");
-            }
-            else
-            {
-                return new()
-                {
-                    HostName = hostname,
-                    ConnectService = new ConnectService(),
-                    IpService = new IpService(new HttpClient())
-                };
-            }
-        }
+            HostName = hostname,
+            ConnectService = new ConnectService(),
+            IpService = new IpService(new HttpClient())
+        };
     }
 }
