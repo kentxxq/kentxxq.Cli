c2f9f58144e74e705a70c5e1eb6dca1bcce6738e
kentxxq <805429509@qq.com>
2022-02-20 02:26:09 +0800

    feat: 完成redis命令

--- src/Cli/Commands/ken-redis/RedisCommand.cs
+++ src/Cli/Commands/ken-redis/RedisCommand.cs
@@ -1,55 +1,83 @@
-﻿using Spectre.Console;
-using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.CommandLine;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Spectre.Console;
+using StackExchange.Redis;
 
-namespace Cli.Commands.ken_redis
+namespace Cli.Commands.ken_redis;
+
+public static class RedisCommand
 {
-    public class RedisCommand
+    private static readonly Argument<string> Url = new("url", "url: redis.com");
+
+    private static readonly Option<int> Port = new(new[] { "-port", "--serverPort" }, () => 6379, "default:6379");
+
+    private static readonly Option<int> Db = new(new[] { "-db", "--database" }, () => 0, "default:0");
+
+    private static readonly Option<string> Password = new(new[] { "-p", "--password" }, () => "", "default empty");
+
+    public static Command GetCommand()
     {
-        private static readonly Argument<string> url = new Argument<string>("url","url: redis.com");
-
-        private static readonly Option<int> port = new(new[] { "-port", "--serverPort" }, () => 6379, "default:6379");
-
-        private static readonly Option<int> db = new(new[] { "-db", "--database" }, () => 0, "default:0"); 
-
-        private static readonly Option<string> password = new(new[] { "-p", "--password" }, () => "", "default empty");
-        public static Command GetCommand()
+        var command = new Command("redis", "redis")
         {
-            var command = new Command("redis", "redis")
-            {
-                url,
-                port,
-                db,
-                password
-            };
-            command.SetHandler<string,int,int,string>(Run,url,port,db,password);
-            return command;
-        }
+            Url,
+            Port,
+            Db,
+            Password
+        };
+        command.SetHandler<string, int, int, string, CancellationToken>(Run, Url, Port, Db, Password);
+        return command;
+    }
 
 
-        public static async Task<string> Run(string url,int port,int db,string password)
+    private static void Run(string url, int port, int db, string password, CancellationToken ct)
+    {
+        var redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
         {
-            var redis = await ConnectionMultiplexer.ConnectAsync(new ConfigurationOptions
+            EndPoints = { $"{url}:{port}" }
+        });
+        var dbc = redis.GetDatabase(db);
+        var pong = dbc.Ping();
+        AnsiConsole.MarkupLine($"connect [green]success[/],take {pong.TotalMilliseconds} ms");
+
+        var server = redis.GetServer(url, port);
+        var keysCount = server.Keys(db, "*").Count();
+        Console.WriteLine($"db{db} keys:{keysCount}");
+
+        while (!ct.IsCancellationRequested)
+        {
+            Console.Write(">>");
+            var input = Console.ReadLine() ?? "";
+            var inputs = input.Split(" ", 2);
+            if (inputs.Length == 1 && input != "" && input != "exit()")
             {
-                EndPoints = { $"bwd.kentxxq.com:6379" }
-            });
-            var dbc = redis.GetDatabase(db);
-            var pong = await dbc.PingAsync();
-            AnsiConsole.MarkupLine($"connect [green]success[/],take {pong.TotalMilliseconds} ms");
-            
-            var server = redis.GetServer(url, port);
-            var keysCount = server.Keys(db, "*").Count();
-            Console.WriteLine($"db的key数量:{keysCount}");
-            Console.Write(">> ");
-
-
-            await Task.Delay(1000);
-            return "123";
+                var keys = server.Keys(db, input);
+                AnsiConsole.MarkupLine($"[green]keys count:{keys.Count()}[/]");
+            }
+            else if (input == "")
+            {
+                Console.WriteLine("usage:");
+                Console.WriteLine($"a*:search db{db} all keys start with a");
+                Console.WriteLine($"del a2*:delete db{db} all keys start with a2");
+                Console.WriteLine("exit():just exit");
+            }
+            else
+            {
+                switch (inputs[0])
+                {
+                    case "del":
+                        var delKeys = server.Keys(db, inputs[1]);
+                        var delKeysCount = dbc.KeyDelete(delKeys.ToArray());
+                        AnsiConsole.MarkupLine($"[red]deleted {delKeysCount} key(s)[/]");
+                        break;
+                    case "exit()":
+                        return;
+                    default:
+                        Console.WriteLine("unknown command");
+                        break;
+                }
+            }
         }
     }
 }
--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,17 +1,16 @@
-﻿using System.CommandLine;
-using System.CommandLine.Builder;
+﻿using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
+using Cli.Commands;
 
-
-var result = await Cli.Commands.AllCommands.BuildCommandLine()
-      .UseDefaults()
-      .Build()
+await AllCommands.BuildCommandLine()
+    .UseDefaults()
+    .Build()
 #if DEBUG
-//.InvokeAsync(new string[] { "ss" });
-//.InvokeAsync(new string[] { "tr", "kentxxq.com" });
-//.InvokeAsync(new string[] { "ws", "wss://ws.kentxxq.com/ws" });
-//.InvokeAsync(new string[] { "sp", "kentxxq.com:443", "-t 2", "-q", });
-.InvokeAsync(new string[] { "redis", "bwd.kentxxq.com" });
+ // .InvokeAsync(new string[] { "ss" });
+ // .InvokeAsync(new[] { "tr", "kentxxq.com" });
+ //.InvokeAsync(new[] { "ws", "wss://ws.kentxxq.com/ws" });
+ // .InvokeAsync(new[] { "sp", "kentxxq.com:443", "-t 2", "-q" });
+ .InvokeAsync(new[] { "redis", "127.0.0.1" });
 #else
       .InvokeAsync(args);
       //.InvokeAsync(new string[] { "tr" });
