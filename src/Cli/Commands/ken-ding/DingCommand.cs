9f126601c9a2141dd39d296479f5c8e729a58f41
kent xxq <805429509@qq.com>
2022-03-21 22:00:13 +0800

    tmp: add TODO

---
+++ src/Cli/Commands/ken-ding/DingCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cli.Commands.ken_ding
+{
+    public static class DingCommand
+    {
+        //TODO 完成加签发送信息
+    }
+}
