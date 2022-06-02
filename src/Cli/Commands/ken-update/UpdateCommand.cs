f06ad5fe6e6b58984dd638a1f81c63fcec39a0c9
kent xxq <805429509@qq.com>
2022-06-02 16:32:10 +0800

    feat: 准备编写update命令

---
+++ src/Cli/Commands/ken-update/UpdateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+
+namespace Cli.Commands.ken_update;
+
+public static class UpdateCommand
+{
+    public static Command GetCommand()
+    {
+        var command = new Command("update", "update ken command");
+        command.SetHandler(Run);
+        return command;
+    }
+
+    private static void Run()
+    {
+        // 检查当前版本
+        // 检查github上面的版本
+        // 下载对应最新的cli
+        // 移动当前的版本，将新版本cli放到现有的位置
+    }
+}
