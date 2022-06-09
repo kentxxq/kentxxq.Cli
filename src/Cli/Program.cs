f6628f4578372772baaa4ab67c946a9b8ee72ee2
KENT XXQ <admin@kentxxq.com>
2022-06-09 20:41:16 +0800

    style: 优化错误输出

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -6,11 +6,12 @@ using Spectre.Console;
 
 await AllCommands.BuildCommandLine()
     .UseDefaults()
+#if !DEBUG
     .UseExceptionHandler((exception, context) =>
     {
-        MyAnsiConsole.MarkupErrorLine("internal error");
-        AnsiConsole.WriteException(exception);
+        MyAnsiConsole.MarkupErrorLine($"{exception.Message}");
     },1)
+#endif
     .Build()
 #if DEBUG
     // .InvokeAsync(new string[] { "ss" });
@@ -18,9 +19,9 @@ await AllCommands.BuildCommandLine()
 // .InvokeAsync(new[] { "ws", "wss://ws.kentxxq.com/ws" });
 // .InvokeAsync(new[] { "sp", "kentxxq.com:443", "-t 2", "-q" });
 // .InvokeAsync(new[] { "redis", "bwd.kentxxq.com","-p didi" });
-    // .InvokeAsync(new[] { "k8s", "get-restarted-pod" });
+    .InvokeAsync(new[] { "k8s", "get-restarted-pod" });
     // .InvokeAsync(new[] { "web" });
-    .InvokeAsync(new[] { "update","-kv","1.2.7" });
+    // .InvokeAsync(new[] { "update","-kv","1.2.7" });
 #else
       .InvokeAsync(args);
       //.InvokeAsync(new string[] { "tr" });
