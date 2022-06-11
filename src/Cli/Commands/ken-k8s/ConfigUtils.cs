9784d4596a6d8a7e35feb83adbeebfff9eddadeb
kent xxq <805429509@qq.com>
2022-06-11 22:14:39 +0800

    feat: 新增资源使用率

---
+++ src/Cli/Commands/ken-k8s/ConfigUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using k8s;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Cli.Commands.ken_k8s;
+
+public static class ConfigUtils
+{
+    public static async Task<KubernetesClientConfiguration> GetConfig(string? configPath)
+    {
+        if (configPath.IsNullOrEmpty())
+        {
+            configPath = KubernetesClientConfiguration.KubeConfigDefaultLocation;
+        }
+        else if (!Path.IsPathRooted(configPath))
+        {
+            configPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE") ?? @"\", @$".kube\{configPath}")
+                : Path.Combine(Environment.GetEnvironmentVariable("HOME") ?? "/", $".kube/{configPath}");
+        }
+
+        var config = await KubernetesClientConfiguration.BuildConfigFromConfigFileAsync(new FileInfo(configPath));
+        return config;
+    }
+}
--- src/Cli/Commands/ken-k8s/K8SCommand.cs
+++ src/Cli/Commands/ken-k8s/K8SCommand.cs
@@ -1,6 +1,8 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Cli.Utils;
 using k8s;
@@ -11,73 +13,24 @@ namespace Cli.Commands.ken_k8s;
 
 public static class K8SCommand
 {
-    private static readonly Argument<string> SubCommand = new("subCommand", "allow: get-restarted-pod");
-
-    private static readonly Option<string> ConfigPath = new(
+    public static readonly Option<string> ConfigPath = new(
         new[] { "-c", "--kubeconfig" },
-        "kubeconfig file path");
+        "kubeconfig file path"
+        );
 
-    private static readonly Option<string> ClusterNamespace =
-        new(new[] { "-n", "--namespace" }, "specified namespace");
+    public static readonly Option<string> ClusterNamespace = new(
+        new[] { "-n", "--namespace" }, 
+        "specified namespace"
+        );
 
     public static Command GetCommand()
     {
-        var command = new Command("k8s", "k8s")
-        {
-            SubCommand,
-            ConfigPath,
-            ClusterNamespace
-        };
-        command.SetHandler(Run, SubCommand, ConfigPath, ClusterNamespace);
+        var command = new Command("k8s", "get k8s resource info");
+        command.AddGlobalOption(ConfigPath);
+        command.AddGlobalOption(ClusterNamespace);
+        
+        command.AddCommand(GetRestartPod.GetCommand());
+        command.AddCommand(ListUsage.GetCommand());
         return command;
-    }
-
-    private static async Task Run(string subCommand, string configPath, string clusterNamespace)
-    {
-        var config = configPath.IsNullOrEmpty()
-            ? KubernetesClientConfiguration.BuildConfigFromConfigFile()
-            : await KubernetesClientConfiguration.BuildConfigFromConfigFileAsync(new FileInfo(configPath));
-        var client = new Kubernetes(config);
-        switch (subCommand)
-        {
-            case "get-restarted-pod":
-                await GetRestartedPod(client, clusterNamespace);
-                return;
-            default:
-                MyAnsiConsole.MarkupErrorLine("command not found!");
-                return;
-        }
-    }
-
-    private static async Task GetRestartedPod(Kubernetes client, string clusterNamespace)
-    {
-        var table = new Table();
-        await AnsiConsole.Live(table).StartAsync(async ctx =>
-        {
-            var namespaces = await client.ListNamespaceAsync();
-            if (!clusterNamespace.IsNullOrEmpty())
-                namespaces.Items = namespaces.Items.Where(n => n.Metadata.Name == clusterNamespace).ToList();
-
-            foreach (var ns in namespaces.Items)
-            {
-                var pods = await client.ListNamespacedPodAsync(ns.Metadata.Name);
-                var restartedPods = pods.Items.Where(p => p.Status.ContainerStatuses.Any(c => c.RestartCount != 0));
-                foreach (var restartedPod in restartedPods)
-                foreach (var c in restartedPod.Status.ContainerStatuses)
-                {
-                    if (table.Rows.Count == 0)
-                    {
-                        table.AddColumn("Namespace");
-                        table.AddColumn("Pod Name");
-                        table.AddColumn("Restart Times");
-                        ctx.Refresh();
-                    }
-
-                    table.AddRow(ns.Metadata.Name, c.Name, c.RestartCount.ToString());
-                }
-            }
-
-            if (table.Rows.Count == 0) table.AddColumn("Not Found! Your pods are healthy.");
-        });
     }
 }
