39f6396581d591c6922507e9d524fe03fe641686
KENT XXQ <admin@kentxxq.com>
2022-07-16 01:44:57 +0800

    fix: 加上一些参数，但是性能和go的有差距！

--- src/Cli/Commands/ken-bm/BenchMarkCommand.cs
+++ src/Cli/Commands/ken-bm/BenchMarkCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
@@ -10,33 +11,64 @@ using Masuit.Tools;
 
 namespace Cli.Commands.ken_bm;
 
+/// <summary>
+/// TODO 性能和hey还是有差距。。。
+/// </summary>
 public class BenchMarkCommand
 {
     /// <summary>
     /// http地址
     /// </summary>
     private static readonly Argument<string> Url = new("url", "url: https://test.kentxxq.com/api/Counter/Count");
+
+    private static readonly Option<int> Duration = new(new string[] { "-d", "--duration"}, () => 10, "duration: benchmark duration");
     
+    private static readonly Option<int> Concurrent = new(new string[] { "-c", "--concurrent"}, () => 50, "concurrent: concurrent request");
+
     private static int count = 0;
 
     public static Command GetCommand()
     {
         var command = new Command("bm","http benchmark")
         {
-            Url
+            Url,
+            Duration,
+            Concurrent
         };
         command.SetHandler(async context =>
         {
             var url = context.ParseResult.GetValueForArgument(Url);
+            var duration = context.ParseResult.GetValueForOption(Duration);
+            var concurrent = context.ParseResult.GetValueForOption(Concurrent);
             // var cts = context.GetCancellationToken();
             var cts = new CancellationTokenSource();
-            cts.CancelAfter(TimeSpan.FromSeconds(5));
-            await Run(url,cts.Token);
+            cts.CancelAfter(TimeSpan.FromSeconds(duration));
+            await Run(url,concurrent,cts.Token);
+            // await Run2(url, cts.Token);
         });
         return command;
     }
 
-    private static async Task Run(string url,CancellationToken cts)
+    // private static async Task Run2(string url, CancellationToken cts)
+    // {
+    //     var client = new HttpClient()
+    //     {
+    //         BaseAddress = new Uri(url)
+    //     };
+    //     var stopWatch = new Stopwatch();
+    //     stopWatch.Start();
+    //     var taskList = new List<Task<string>>();
+    //     for (int i = 0; i < 50000; i++)
+    //     {
+    //         var t = client.GetStringAsync(url,cts);
+    //         taskList.Add(t);
+    //     }
+    //
+    //     await Task.WhenAll(taskList);
+    //     Console.WriteLine($"{stopWatch.ElapsedMilliseconds / 1000} s");
+    // }
+    
+    private static async Task Run(string url,int concurrent,CancellationToken cts)
     {
         var client = new HttpClient()
         {
@@ -44,7 +76,7 @@ public class BenchMarkCommand
         };
         
         var taskList = new List<Task<string>>();
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < concurrent; i++)
         {
             var t = client.GetStringAsync(url,cts);
             taskList.Add(t);
