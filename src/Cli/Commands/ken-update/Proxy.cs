851b27924426501708d81dc2a3c8c6b00df9a19a
kent xxq <805429509@qq.com>
2022-07-23 22:02:38 +0800

    sync

---
+++ src/Cli/Commands/ken-update/Proxy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Cli.Commands.ken_update;
+
+public enum ProxyEnum
+{
+    Ghproxy,
+    Fastgit,
+    Cfworker
+}
+
+
+public static class Proxy
+{
+    public static Dictionary<ProxyEnum, string> data = new()
+    {
+        { ProxyEnum.Ghproxy, "https://ghproxy.com/" },
+        { ProxyEnum.Fastgit, "https://download.fastgit.org/" },
+        { ProxyEnum.Cfworker ,"https://github.abskoop.workers.dev/"}
+    };
+}
