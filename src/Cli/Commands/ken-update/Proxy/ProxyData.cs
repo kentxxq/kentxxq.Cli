31ae2a55dc002835a429c40d5ca967da2f086d44
kent xxq <805429509@qq.com>
2022-07-28 18:27:20 +0800

    sync

---
+++ src/Cli/Commands/ken-update/Proxy/ProxyData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Cli.Commands.ken_update;
+
+
+public enum ProxyEnum
+{
+    Ghproxy,
+    Fastgit,
+    Cfworker
+}
+
+
+// public static class Proxy
+// {
+//     public static Dictionary<ProxyEnum, string> Data = new()
+//     {
+//         { ProxyEnum.Ghproxy, "https://ghproxy.com/" },
+//         { ProxyEnum.Fastgit, "https://download.fastgit.org/" },
+//         { ProxyEnum.Cfworker ,"https://github.abskoop.workers.dev/"}
+//     };
+// }
---
+++ src/Cli/Commands/ken-update/Proxy/ProxyStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cli.Commands.ken_update;
+
+public class ProxyStrategy
+{
+    private IProxyStrategy _proxyStrategy;
+    public ProxyStrategy(ProxyEnum proxy)
+    {
+        var t = (IProxyStrategy)Activator.CreateInstance(null,"Cli.Commands.ken_update.Proxy."+proxy);
+        _proxyStrategy = t;
+    }
+
+    public string GetDownloadUrl(ProxyEnum proxy,string url)
+    {
+        return _proxyStrategy.GetProxyUrl(url);
+    }
+}
