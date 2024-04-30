105dca10125e4427fa9227a1d55a50e3734f7fd2
kentxxq <805429509@qq.com>
2024-04-30 17:18:24 +0800

    redis对key进行slot分组后删除. 避免报错

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -12,9 +12,9 @@ await AllCommands.BuildCommandLine()
     // .InvokeAsync(new[] { "tr", "baidu.com" });
 // .InvokeAsync(new[] { "ws", "wss://ws.kentxxq.com/ws" });
 // .InvokeAsync(new[] { "sp", "kentxxq.com:443", "-t 2","-n 5" });
-// .InvokeAsync(new[] { "redis", "a.kentxxq.com","-p didi2" });
+.InvokeAsync(new[] { "redis", "redis-shzlpfwpcf3gjserz.redis.volces.com","-p","Z6&WaM9xV[ge" });
 // .InvokeAsync(new[] { "k8s", "2" });
-.InvokeAsync(new[] { "wp", "https://test.kentxxq.com/api/Delay/1500","-t 2","-i 5" });
+// .InvokeAsync(new[] { "wp", "https://test.kentxxq.com/api/Delay/1500","-t 2","-i 5" });
 // .InvokeAsync(new[] { "bm", "https://test.kentxxq.com/api/Counter/count","-c 5","-d 3","-fcurl.txt" });
 // .InvokeAsync(new[] { "bm", "http://127.0.0.1:5000/" });
 // .InvokeAsync(new[] { "web" });
